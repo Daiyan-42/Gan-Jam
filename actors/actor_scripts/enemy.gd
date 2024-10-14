37a77a1d1dc98fbec67b770ea1391c794c6b902a
GawwyG <154411548+GawwyG@users.noreply.github.com>
2024-10-14 21:31:23 +0600

    basic enemy done

---
+++ actors/actor_scripts/enemy.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+
+@export var SPEED : float = 50
+var direction : int = 0
+var player_nearby : bool = false
+@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	#Sprite flipping
+	
+	if direction == 1:
+		sprite.flip_h = false
+	elif direction == -1:
+		sprite.flip_h = true
+
+	# Motion
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_detection_zone_body_entered(body):
+	if body.name == "Player":
+		player_nearby = true
+		print("attacking")
+		if self.position.x > body.position.x:
+			direction = -1
+		elif self.position.x < body.position.x:
+			direction = 1
+			
+	pass # Replace with function body.
+
+
+func _on_detection_zone_body_exited(body):
+	if body.name == "Player":
+		player_nearby = false
+		print("resetting")
+		direction = 0
+	pass # Replace with function body.
--- actors/actor_scripts/player.gd
+++ actors/actor_scripts/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
-@onready var sprite = $AnimatedSprite2D
+@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
